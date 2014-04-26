69099566d763bd4f545c2a9bb211210b605d72b1
Sergii Bomko <aquila.ua@gmail.com>
2014-04-26 15:13:52 +0300

    front-end mockup

--- src/nH.Web/Controllers/nHController.cs
+++ src/nH.Web/Controllers/nHController.cs
@@ -1,20 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Web.Http;
 
 namespace nH.Web.Controllers
 {
 	public class nHController : ApiController
 	{
-		// GET api/entries
 		public IEnumerable<string> Get()
 		{
-			return new string[] { "value1", "value2" };
-		}
-
-		// GET api/entries/5
-		public string Get(int id)
-		{
-			return "value";
+			return new []
+			{
+				DateTime.Now.ToString(CultureInfo.InvariantCulture)
+			};
 		}
 	}
 }
--- src/nH.Web/Scripts/app/nH.js
+++ src/nH.Web/Scripts/app/nH.js
@@ -1,1 +1,66 @@
-﻿
+﻿window.nHApp = window.nHApp || { };
+
+window.nHApp.datacontext = (function () {
+
+	var datacontext = {
+		getList: getList
+	};
+
+	return datacontext;
+
+	function getList(listObservable, errorObservable) {
+		return ajaxRequest("get", listUrl())
+			.done(getSucceeded)
+			.fail(getFailed);
+
+		function getSucceeded(data) {
+			$.each(data, function() {
+				listObservable.push(this);
+			});
+		}
+
+		function getFailed() {
+			errorObservable("Error retrieving list.");
+		}
+	}
+
+	function ajaxRequest(type, url, data, dataType) {
+		var options = {
+			dataType: dataType || "json",
+			contentType: "application/json",
+			cache: false,
+			type: type,
+			data: data ? data.toJson() : null
+		};
+		var antiForgeryToken = $("#antiForgeryToken").val();
+		if (antiForgeryToken) {
+			options.headers = {
+				'RequestVerificationToken': antiForgeryToken
+			}
+		}
+		return $.ajax(url, options);
+	}
+
+	function listUrl(id) { return "/api/nH/" + (id || ""); }
+})();
+
+window.nHApp.nHViewModel = (function (ko, datacontext) {
+	var list = ko.observableArray(),
+		error = ko.observable();
+
+	datacontext.getList(list, error);
+
+	return {
+		list: list,
+		update: update
+	};
+
+	function update() {
+		console.log(datacontext);
+		datacontext.getList(list, error);
+		window.setTimeout(update, 2000);
+	}
+})(ko, nHApp.datacontext);
+
+window.setTimeout(window.nHApp.nHViewModel.update, 2000);
+ko.applyBindings(window.nHApp.nHViewModel);
