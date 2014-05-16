79c595f9e68b21b532eb50e5b7c6a0c630604a6f
Sergii Bomko <aquila.ua@gmail.com>
2014-05-16 12:01:13 +0300

    added stream controller as a default

--- src/nH.Web/App_Start/RouteConfig.cs
+++ src/nH.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace nH.Web
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Stream", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
