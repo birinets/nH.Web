68205cee2e5049210f0b3f93763d605b4b4eb5a8
Sergii Bomko <aquila.ua@gmail.com>
2014-05-16 12:01:49 +0300

    added stream controller as a default

---
+++ src/nH.Web/Controllers/StreamController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace nH.Web.Controllers
+{
+	public class StreamController : Controller
+	{
+		public ActionResult Index(string returnUrl)
+		{
+			ViewBag.ReturnUrl = returnUrl;
+			return View();
+		}
+	}
+}
